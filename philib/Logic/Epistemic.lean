c7e3186a9424eed3440f274cd23a7f9043926d72
栀染 <imbue2025@outlook.com>
2025-10-24 22:49:38 +0800

    feat(logic): implement basic epistemic logic

---
+++ philib/Logic/Epistemic.lean
@@ -0,0 +1,27 @@
+import Philib.Logic.Modal
+namespace Philib.Logic.Epistemic
+variable (A : Type u) -- Type of Agents
+variable (W : Type v) -- Type of Worlds
+structure EpistemicFrame where
+  R : A → W → W → Prop
+variable (frame : EpistemicFrame A W)
+abbrev KripkeProp := Philib.Logic.KripkeProp W
+def knows (a : A) (p : KripkeProp) : KripkeProp :=
+  fun w => ∀ w' : W, frame.R a w w' → p w'
+def believes (a : A) (p : KripkeProp) : KripkeProp :=
+  fun w => ∀ w' : W, frame.R a w w' → p w'
+notation:80 a " Knows " p => knows a p
+notation:80 a " Believes " p => believes a p
+open Philib.Logic (IsValid)
+theorem axiom_T (h_refl : ∀ a w, frame.R a w w) (a : A) (p : KripkeProp) :
+  IsValid ((a Knows p) → p) :=
+begin
+  intro w,      -- For any world w...
+  intro h_knows,  -- Assume `a` knows `p` at `w`.
+  unfold knows at h_knows,
+  -- `h_knows` is now `∀ w', frame.R a w w' → p w'`.
+  -- Since the relation is reflexive, `frame.R a w w` is true.
+  let h_p_at_w := h_knows w (h_refl a w),
+  exact h_p_at_w,
+end
+end Philib.Logic.Epistemic
